47c42ac7285c2cf1320fff8999a55b005de70296
Megha1202 <85835712+Megha1202@users.noreply.github.com>
2021-06-26 15:17:27 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("kapil", "sharma")
+
+
+
+
+def my_func(x):
+  return 5 * x
+
+print(my_func(3))
+print(my_func(5))
+print(my_func(9))
